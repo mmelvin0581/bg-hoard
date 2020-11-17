4d2e535ce7ce8526c63d3ce35d74760f150df6fd
Michael Melvin <michael.melvin@pwc.com>
2020-11-17 13:30:00 -0500

    lab 2 complete

---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
